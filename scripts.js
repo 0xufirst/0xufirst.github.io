66ffd078dbce7d3019452c4bf1252c1ef5550b34
0xufirst <153159998+0xufirst@users.noreply.github.com>
2025-03-02 03:03:04 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Bem-vindo ao meu site pessoal!');
+
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
